1d778461022dc0327b9c75baf0ac7d8b05e969d8
Evg-Ko <31292566+9420128@users.noreply.github.com>
2021-03-27 22:40:25 +0400

    parcelCleanPaths

---
+++ src/layouts/header.js
@@ -0,0 +1,26 @@
+export default `
+   <!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!-- <link rel="icon" href="img/favicon/favicon.ico"> -->
+        <title>Калькулятор отделки балкона</title>
+        <!--        <link-->
+        <!--            rel="stylesheet"-->
+        <!--            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"-->
+        <!--            integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"-->
+        <!--            crossorigin="anonymous"-->
+        <!--        />-->
+        <!-- <link rel="stylesheet" href="styles/main.css" /> -->
+        <!-- <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"
+        ></script> -->
+        <!-- <script src="data.js"></script> -->
+    </head>
+
+    <body>
+`.trim()
